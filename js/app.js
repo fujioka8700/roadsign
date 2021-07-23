eb5d8de9eec9416e793d37b7be486e385acb4fe6
fujioka8700 <fujioka8700@gmail.com>
2021-07-24 07:31:51 +0900

    div element creation

--- js/app.js
+++ js/app.js
@@ -4,3 +4,26 @@ $('select').change(function () {
     $('section').fadeOut();
     $('section#' + val).fadeIn();
 });
+
+const select = document.querySelector('select');
+select.addEventListener('change', function () {
+    console.log(select.value);
+    $.getJSON('../sign.json', (data) => {
+        for (const property in data) {
+            let newElement = document.createElement("div");
+            let newImageElement = document.createElement("img");
+            let newContent = document.createElement("h2");
+
+            if ('red-circle' === data[property].category) {
+                newImageElement.setAttribute('src', `../images/${data[property].filename}`);
+                newContent.textContent = data[property].name;
+
+                newElement.appendChild(newImageElement);
+                newElement.appendChild(newContent);
+
+                newElement.setAttribute("class", "item");
+                console.log(newElement);
+            }
+        }
+    });
+});
